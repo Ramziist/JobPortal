66a5715748e450c3e5f20d38d5bee5e783c8e758
Ramziist <68219287+Ramziist@users.noreply.github.com>
2020-11-15 19:57:45 +0200

    123

---
+++ JS/javascript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#search').click(function() {
+        $('.nav-link').toggleClass('hide-link');
+        $('.search-form').toggleClass('activeS');
+    });
+});
